a3ceaf87fcf85774bf9e98ea497a63d2c5138a96
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-06-01 07:24:07 -0800

    update

---
+++ lib/widgets/expandable_text.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/dimension.dart';
+
+class ExpandableText extends StatefulWidget {
+  const ExpandableText({Key? key}) : super(key: key);
+
+  get text => String;
+
+  @override
+  State<ExpandableText> createState() => _ExpandableTextState();
+}
+
+class _ExpandableTextState extends State<ExpandableText> {
+  late String firstHalf;
+  late String secondHalf;
+
+  bool hiddenText = true;
+
+  double textHeight = Dimension.screenHeight / 5.63;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.text.length > textHeight) {
+      firstHalf = widget.text.substring(0, textHeight.toInt());
+      secondHalf =
+          widget.text.substring(textHeight.toInt() + 1, widget.text.length);
+    } else {
+      firstHalf = widget.text;
+      secondHalf = "";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
