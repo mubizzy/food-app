e411203ef1344625a7965fffb10b79756e1dea43
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-06-01 19:00:51 -0800

    update

--- lib/pages/food/recommended_food_detail.dart
+++ lib/pages/food/recommended_food_detail.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/utils/dimension.dart';
+import 'package:food_delivery/widgets/app_icon.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/expandable_text.dart';
 
 class RecommendeFoodDetail extends StatelessWidget {
   const RecommendeFoodDetail({Key? key}) : super(key: key);
@@ -9,13 +14,75 @@ class RecommendeFoodDetail extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            toolbarHeight: 70,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(icon: Icons.clear),
+                AppIcon(icon: Icons.shopping_cart_checkout_outlined),
+              ],
+            ),
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(20),
+              child: Container(
+                // margin: EdgeInsets.only(
+                //     left: Dimension.width20, right: Dimension.width20),
+                child: Center(child: BigText(size: 20, text: "Chinese Side")),
+                width: double.maxFinite,
+                padding: EdgeInsets.only(top: 5, bottom: 10),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(Dimension.radius20),
+                        topRight: Radius.circular(Dimension.radius20)),
+                    color: Colors.white),
+              ),
+            ),
             expandedHeight: 300,
+            pinned: true,
+            backgroundColor: AppColors.yellowColor,
             flexibleSpace: FlexibleSpaceBar(
               background: Image.asset(
-                "assets/image/food11.png",
+                "assets/image/food0.png",
                 width: double.maxFinite,
                 fit: BoxFit.cover,
               ),
+            ),
+          ),
+          SliverToBoxAdapter(
+              child: Column(
+            children: [
+              Container(
+                child: ExpandableText(
+                    text:
+                        "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc"),
+              )
+            ],
+          ))
+        ],
+      ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+                left: Dimension.width20 * 2.5,
+                right: Dimension.width20 * 2.5,
+                top: Dimension.height10,
+                bottom: Dimension.height10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(
+                    iconSize: Dimension.iconSize16,
+                    backgroundColor: AppColors.mainColor,
+                    iconColor: Colors.white,
+                    icon: Icons.remove),
+                AppIcon(
+                    iconSize: Dimension.iconSize16,
+                    backgroundColor: AppColors.mainColor,
+                    iconColor: Colors.white,
+                    icon: Icons.add),
+              ],
             ),
           )
         ],
