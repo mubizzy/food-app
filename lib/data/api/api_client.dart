41373f97b5f29b400a0ed675450b3df586074261
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-06-02 21:38:44 -0800

    update

--- lib/data/api/api_client.dart
+++ lib/data/api/api_client.dart
@@ -7,10 +7,21 @@ class ApiClient extends GetConnect implements GetxService {
   late Map<String, String> _mainHeaders;
   ApiClient({required this.appBaseUrl}) {
     baseUrl = appBaseUrl;
+
     timeout = Duration(seconds: 30);
     _mainHeaders = {
       'Content-type': 'application/json ;charset=UTF-8',
       'Authorization': 'Bearer $token',
     };
+  }
+  Future<Response> getData(
+    String uri,
+  ) async {
+    try {
+      Response response = await get(uri);
+      return response;
+    } catch (e) {
+      return Response(statusCode: 1, statusText: e.toString());
+    }
   }
 }
