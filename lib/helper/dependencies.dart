b9b7420267271c665d0d6fbdaaccc603b8b33cf2
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-06-03 22:15:32 -0800

    update

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,6 +1,8 @@
 import 'package:food_delivery/controller/popular_product_controller.dart';
+import 'package:food_delivery/controller/recommended_product_controller.dart';
 import 'package:food_delivery/data/api/api_client.dart';
 import 'package:food_delivery/data/repository/popular_product_repo.dart';
+import 'package:food_delivery/data/repository/recommended_product_repo.dart';
 import 'package:food_delivery/utils/app_constants.dart';
 import 'package:get/get.dart';
 
@@ -10,7 +12,10 @@ Future<void> init() async {
 
   // Repos
   Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
+  Get.lazyPut(() => RecommendedProductRepo(apiClient: Get.find()));
 
   // controllers
   Get.lazyPut(() => PopularProductContoller(popularProductRepo: Get.find()));
+  Get.lazyPut(
+      () => RecommendedProductContoller(recommendedProductRepo: Get.find()));
 }
