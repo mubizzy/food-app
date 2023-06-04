cc532ffabe94e599c41023654f2808a872104c5c
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-06-03 22:15:40 -0800

    update

--- lib/controller/popular_product_controller.dart
+++ lib/controller/popular_product_controller.dart
@@ -10,6 +10,9 @@ class PopularProductContoller extends GetxController {
   List<dynamic> _popularProductList = [];
   List<dynamic> get popularProductList => _popularProductList;
 
+  bool _isLoaded = false;
+  bool get isLoaded => _isLoaded;
+
   Future<void> getPopularProductList() async {
     Response response = await popularProductRepo.getPopularProductList();
     if (response.statusCode == 200) {
@@ -18,6 +21,7 @@ class PopularProductContoller extends GetxController {
       _popularProductList
           .addAll(Product.fromJson(response.body).products as Iterable);
       // print(_popularProductList);
+      _isLoaded = true;
       update();
     } else {}
   }
