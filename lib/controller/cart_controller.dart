ff290a11c1fd41a8c8fd7cd6f09aa37ee5027769
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-06-04 19:23:15 -0800

    update

---
+++ lib/controller/cart_controller.dart
@@ -0,0 +1,10 @@
+import 'package:food_delivery/data/repository/cart_repo.dart';
+import 'package:get/get.dart';
+
+class CartController extends GetxController {
+  final CartRepo cartRepo;
+
+  CartController({
+    required this.cartRepo,
+  });
+}
