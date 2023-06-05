903fb77c2448e60d817421fe68e370f902b65377
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-06-04 19:02:36 -0800

    update

--- lib/pages/home/main_food_page.dart
+++ lib/pages/home/main_food_page.dart
@@ -19,7 +19,7 @@ class _MainFoodPageState extends State<MainFoodPage> {
         children: [
           Container(
             child: Container(
-                margin: EdgeInsets.only(top: 65, bottom: 15),
+                margin: EdgeInsets.only(top: 35, bottom: 10),
                 padding: EdgeInsets.only(left: 20, right: 20),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
