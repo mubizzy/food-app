d24539f61e015542c3339afaaa3a457efcdf6f37
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-06-08 18:03:54 -0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/controller/popular_product_controller.dart';
+import 'package:food_delivery/controller/recommended_product_controller.dart';
+import 'package:food_delivery/pages/home/food_page_body.dart';
 import 'package:food_delivery/pages/home/main_food_page.dart';
 import 'package:get/get.dart';
 
 import 'pages/food/popular_food_detail.dart';
 import 'pages/food/recommended_food_detail.dart';
+import "helper/dependencies.dart" as dep;
+import 'routes/route_helper.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
+
   runApp(const MyApp());
 }
 
@@ -15,12 +23,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Get.find<PopularProductContoller>().getPopularProductList();
+    Get.find<RecommendedProductContoller>().getRecommendedProductList();
     return GetMaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: RecommendeFoodDetail());
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainFoodPage(),
+      getPages: RouteHelper.routes,
+    );
   }
 }
