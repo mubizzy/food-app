910373db77cb19bd2a8a07769451da9ca4b94c25
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-06-08 18:03:42 -0800

    update

--- lib/controller/cart_controller.dart
+++ lib/controller/cart_controller.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:food_delivery/data/repository/cart_repo.dart';
+import 'package:food_delivery/models/cart_model.dart';
+import 'package:food_delivery/models/popular_product_model.dart';
+import 'package:food_delivery/utils/colors.dart';
 import 'package:get/get.dart';
 
 class CartController extends GetxController {
@@ -7,4 +11,85 @@ class CartController extends GetxController {
   CartController({
     required this.cartRepo,
   });
+  Map<int, CartModel> _items = {};
+  Map<int, CartModel> get items => _items;
+
+// cart functionality
+  void addItem(ProductModel product, int quantity) {
+    // print("length of the item is" + _items.length.toString());
+    var totalQuantity = 0;
+    if (_items.containsKey(product.id!)) {
+      _items.update(product.id!, (value) {
+        totalQuantity = value.quantity! + quantity;
+        return CartModel(
+          id: value.id,
+          name: value.name,
+          price: value.price,
+          quantity: value.quantity! + quantity,
+          img: value.img,
+          isExist: true,
+          time: DateTime.now().toString(),
+        );
+      });
+
+      if (totalQuantity <= 0) {
+        _items.remove(product.id);
+      }
+    } else {
+      if (quantity > 0) {
+        _items.putIfAbsent(product.id!, () {
+          // print("adding item to the cart " +
+          //     product.id!.toString() +
+          //     " quantity " +
+          //     quantity.toString());
+          _items.forEach((key, value) {
+            // print("Quantity is " + value.quantity.toString());
+          });
+          return CartModel(
+            id: product.id,
+            name: product.name,
+            price: product.price,
+            quantity: quantity,
+            img: product.img,
+            isExist: true,
+            time: DateTime.now().toString(),
+          );
+        });
+      } else {
+        Get.snackbar(
+          "item count",
+          "you should at least add an item in the cart !",
+          backgroundColor: AppColors.mainColor,
+          colorText: Colors.white,
+        );
+      }
+    }
+  }
+
+  bool existInCart(ProductModel product) {
+    if (_items.containsKey(product.id)) {
+      return true;
+    }
+    return false;
+  }
+
+  int getQuantity(ProductModel product) {
+    var quantity = 0;
+    if (_items.containsKey(product.id)) {
+      _items.forEach((key, value) {
+        if (key == product.id) {
+          quantity = value.quantity!;
+        }
+      });
+    }
+    return quantity;
+  }
+
+  int get totalItems {
+    var totalQuantity = 0;
+    _items.forEach((key, value) {
+      totalQuantity += value.quantity!;
+    });
+    return totalQuantity;
+  }
 }
