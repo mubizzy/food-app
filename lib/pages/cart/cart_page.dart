ea9f28fd0fc557fb5520613880b0e2a145b799ae
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-06-30 00:01:56 +0100

    cart page

---
+++ lib/pages/cart/cart_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/utils/dimension.dart';
+import 'package:food_delivery/widgets/app_icon.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+              top: Dimension.height20 * 3,
+              left: Dimension.width20,
+              right: Dimension.width20,
+              child: Row(
+                children: [
+                  AppIcon(
+                    icon: Icons.arrow_back_ios,
+                    iconColor: Colors.white,
+                    backgroundColor: AppColors.mainColor,
+                    iconSize: Dimension.iconSize24,
+                  )
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+}
