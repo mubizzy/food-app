edde2e0b23339828dbe74e177182e24b1eb6a004
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-06-30 01:51:18 +0100

    cart changes

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/utils/dimension.dart';
 import 'package:food_delivery/widgets/app_icon.dart';
+import 'package:food_delivery/widgets/big_text.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -47,19 +48,52 @@ class CartPage extends StatelessWidget {
               right: Dimension.width20,
               bottom: 0,
               child: Container(
+                margin: EdgeInsets.only(top: Dimension.height15),
                 color: Colors.red,
-                child: ListView.builder(
-                    itemCount: 10,
-                    itemBuilder: (_, index) {
-                      return Container(
-                        height: 100,
-                        width: 100,
-                        color: Colors.blue,
-                        margin: EdgeInsets.only(
-                          bottom: 10,
-                        ),
-                      );
-                    }),
+                child: MediaQuery.removePadding(
+                  context: context,
+                  removeTop: true,
+                  child: ListView.builder(
+                      itemCount: 10,
+                      itemBuilder: (_, index) {
+                        return Container(
+                          height: Dimension.height20 * 5,
+                          width: double.maxFinite,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: Dimension.height20 * 5,
+                                height: Dimension.height20 * 5,
+                                margin:
+                                    EdgeInsets.only(bottom: Dimension.height10),
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image:
+                                          AssetImage("assets/image/food0.png"),
+                                    ),
+                                    borderRadius: BorderRadius.circular(
+                                        Dimension.radius20),
+                                    color: Colors.white),
+                              ),
+                              SizedBox(
+                                width: Dimension.width10,
+                              ),
+                              Expanded(
+                                  child: Container(
+                                height: Dimension.height20 * 5,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [BigText(text: "text")],
+                                ),
+                              ))
+                            ],
+                          ),
+                        );
+                      }),
+                ),
               ))
         ],
       ),
