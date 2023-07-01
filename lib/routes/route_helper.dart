1871f9038b4a15f2ec82d07a3d9b3b16e235eef4
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-07-01 13:57:28 +0100

    addition of splash to router

--- lib/routes/route_helper.dart
+++ lib/routes/route_helper.dart
@@ -1,33 +1,44 @@
 import 'package:food_delivery/pages/cart/cart_page.dart';
 import 'package:food_delivery/pages/food/popular_food_detail.dart';
 import 'package:food_delivery/pages/food/recommended_food_detail.dart';
+import 'package:food_delivery/pages/home/home_page.dart';
 import 'package:food_delivery/pages/home/main_food_page.dart';
+import 'package:food_delivery/pages/splash/splash_page.dart';
 import 'package:get/get.dart';
 
 class RouteHelper {
+  static const String splashPage = "/splash-page";
   static const String initial = "/";
   static const String popularFood = "/popular-food";
   static const String recommendedFood = "/recommended-food";
   static const String cartPage = "/cart-page";
 
-  static String getInitial() => initial;
+  static String getInitial() => '$initial';
+  static String getsplashPage() => '$splashPage';
   static String getPopularFood(int pageId, String page) =>
       '$popularFood?pageId=$pageId&page=$page';
-  static String getRecommendedFood(int pageId) =>
-      '$recommendedFood?pageId=$pageId';
+  static String getRecommendedFood(int pageId, String page) =>
+      '$recommendedFood?pageId=$pageId&page=$page';
   static String getCartPage() => '$cartPage';
 
   static List<GetPage> routes = [
     GetPage(
+      name: splashPage,
+      page: () => SplashScreen(),
+    ),
+    GetPage(
       name: initial,
-      page: () => MainFoodPage(),
+      page: () => HomePage(),
     ),
     GetPage(
       name: popularFood,
       page: () {
         var pageId = Get.parameters['pageId'];
         var page = Get.parameters['page'];
-        return PopularFoodDetail(pageId: int.parse(pageId!), page: page!);
+        return PopularFoodDetail(
+          pageId: int.parse(pageId!),
+          page: page!,
+        );
       },
       transition: Transition.fadeIn,
     ),
@@ -35,7 +46,8 @@ class RouteHelper {
       name: recommendedFood,
       page: () {
         var pageId = Get.parameters['pageId'];
-        return RecommendedFoodDetail(pageId: int.parse(pageId!));
+        var page = Get.parameters['page'];
+        return RecommendedFoodDetail(pageId: int.parse(pageId!), page: page!);
       },
       transition: Transition.fadeIn,
     ),
